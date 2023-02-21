6ff12b65d15f581151f80a99b41968419e84a2ac
Faiz981219 <94294984+Faiz981219@users.noreply.github.com>
2023-02-22 00:56:01 +0800

    Update astronomic calculator(sun ephemeris).py

--- astronomic calculator(sun ephemeris).py
+++ astronomic calculator(sun ephemeris).py
@@ -193,6 +193,7 @@ while True:
     print('Latitude: '+str(Latitude))
     print('Longitude: '+str(Longitude))
     print('Location: ' + location.address)
+    print('Date & Time Calculation: ' +Year + '-' + Month + '-' + Second + '' + Hour + ':' + Minute + ':' + Second)
     print('\nGMST: '+str(Greanwitch_Mean_Sideral_Time_d))
     print("Sun's Mean Longitude: " + str(Sun_Mean_Longitude_d))
     print('Perihelion Longitude: ' +str(Perihelion_Longitude_d))
